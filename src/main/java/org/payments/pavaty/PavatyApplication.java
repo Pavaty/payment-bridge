43e82fb841f1a5a8759432e70f648f8c71538d88
Shreyank Byadagi <shreyank@Shreyanks-MacBook-Pro-2.local>
2022-07-07 10:45:03 +0530

    initial code template

---
+++ src/main/java/org/payments/pavaty/PavatyApplication.java
@@ -0,0 +1,13 @@
+package org.payments.pavaty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PavatyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PavatyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/payments/pavaty/PavatyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.payments.pavaty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PavatyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
